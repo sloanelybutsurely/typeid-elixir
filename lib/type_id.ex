e4e25935b71cb6b5e2525c05d2b310381e583da2
Sloane Perrault <sloane@perrault.email>
2023-06-29 15:20:23 -0400

    `mix new`

---
+++ lib/type_id.ex
@@ -0,0 +1,18 @@
+defmodule TypeID do
+  @moduledoc """
+  Documentation for `TypeID`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TypeID.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/type_id_test.exs
@@ -0,0 +1,8 @@
+defmodule TypeIDTest do
+  use ExUnit.Case
+  doctest TypeID
+
+  test "greets the world" do
+    assert TypeID.hello() == :world
+  end
+end
