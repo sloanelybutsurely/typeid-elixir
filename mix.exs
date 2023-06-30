4a243685dd296143b58b10d70b2841a6d8679e47
Sloane Perrault <sloane@perrault.email>
2023-06-30 07:03:50 -0400

    add project information

--- mix.exs
+++ mix.exs
@@ -3,25 +3,37 @@ defmodule TypeID.MixProject do
 
   def project do
     [
-      app: :typeid,
+      app: :typeid_elixir,
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "TypeID Elixir",
+      source_url: "https://github.com/sloanelybutsurely/typeid-elixir"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
+    [extra_applications: [:logger]]
+  end
+
+  defp description do
+    "A type-safe, K-sortable, globally unique identifier inspired by Stripe IDs"
+  end
+
+  defp package do
     [
-      extra_applications: [:logger]
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/sloanelybutsurely/typeid-elixir"}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:uniq, "~> 0.5.4"}
+      {:uniq, "~> 0.5.4"},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
     ]
   end
 end
