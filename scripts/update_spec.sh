a9186592d632a8967caee7d4f4d940b872681c3f
Sloane Perrault <sloane@perrault.email>
2023-07-06 10:22:31 -0400

    add script to update spec, update spec files

---
+++ scripts/update_spec.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash -ex
+
+wget https://raw.githubusercontent.com/jetpack-io/typeid/main/spec/invalid.yml -O priv/spec/invalid.yml
+wget https://raw.githubusercontent.com/jetpack-io/typeid/main/spec/valid.yml -O priv/spec/valid.yml
